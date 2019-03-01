6aa03f0fc1378f18c21f8390c169b474d0bf4f11
riyajose93 <37532740+riyajose93@users.noreply.github.com>
2019-03-01 16:52:18 +0530

    Add files via upload

---
+++ ASP.NET MVC-db/Controllers/CustController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using mvcdbapp.Models;
+
+namespace mvcdbapp.Controllers
+{
+    public class CustController : Controller
+    {
+        private CustomerContext db = new CustomerContext();
+
+        // GET: /Cust/
+        public ActionResult Index()
+        {
+            return View(db.Customers.ToList());
+        }
+
+        // GET: /Cust/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // GET: /Cust/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Cust/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="CustomerId,name,address,email,uname,pwd")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Customers.Add(customer);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(customer);
+        }
+
+        // GET: /Cust/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // POST: /Cust/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="CustomerId,name,address,email,uname,pwd")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(customer).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(customer);
+        }
+
+        // GET: /Cust/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // POST: /Cust/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Customer customer = db.Customers.Find(id);
+            db.Customers.Remove(customer);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ASP.NET MVC-db/Models/Customer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace mvcdbapp.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        [Required]
+        [Display(Name="Name")]
+        
+        public string name { get; set; }
+        [Required]
+        [Display(Name="Address")]
+        [DataType(DataType.MultilineText)]
+        public string address { get; set; }
+        [Required]
+        [Display(Name="E-mail")]
+        [DataType(DataType.EmailAddress)]
+        public string email { get; set; }
+        [Required]
+        [Display(Name="User Name")]
+        public string uname { get; set; }
+        [Required(ErrorMessage="Must contain 8 characters")]
+        [Display(Name="Password")]
+        [DataType(DataType.Password)]
+        public string pwd { get; set; }
+    }
+    
+}
